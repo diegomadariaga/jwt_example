65a69d447ec4580941417fde2492ad23442588e4
diegomadariaga <diegonicolasmadariaga@gmail.com>
2025-07-10 17:32:37 -0400

    refactor: remove login module and related authentication files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LoginModule } from './login/login.module';
 
 @Module({
-  imports: [LoginModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
