de1948196a832cd6c7c9f82b83e51f56de4b4147
diegomadariaga <diegonicolasmadariaga@gmail.com>
2025-07-10 15:29:07 -0400

    fix: correct password validation logic in AuthUseCase

--- src/login/use-cases/auth.usecase.ts
+++ src/login/use-cases/auth.usecase.ts
@@ -8,9 +8,9 @@ export class AuthUseCase {
 
   async execute(loginDto: LoginDto): Promise<{ accessToken: string }> {
     const validUser = process.env.LOGIN_USER || 'admin';
-    const validName = process.env.LOGIN_NAME || 'admin';
-    if (loginDto.username === validUser && loginDto.password === validName) {
-      const payload = { user: loginDto.username, name: validName };
+    const validPass = process.env.LOGIN_PASS || 'admin';
+    if (loginDto.username === validUser && loginDto.password === validPass) {
+      const payload = { user: loginDto.username, pass: validPass };
       const accessToken = this.jwtService.sign(payload);
       return { accessToken };
     }
